714e9cd9a546dee94f86a76904fe24623df332b9
Avanish08 <ojhaavanish190@gmail.com>
2024-07-18 12:24:38 +0530

    third commit

--- client/src/Component/FormLogin.js
+++ client/src/Component/FormLogin.js
@@ -1,10 +1,13 @@
 import React from 'react'
+// import { Link } from "react-router-dom";
+import '../Css/FormLogin.css';
 
 function FormLogin() {
   return (
     <>
     <div className="nav">
-        <div className="logo"></div>
+        <div className="logo"> <h1>PU</h1></div>
+        <div className="navtext"> <h4>Project Unlocked </h4></div>
     </div>
     <div className="main">
         <div className="left"></div>
@@ -20,6 +23,10 @@ function FormLogin() {
                 <input type="password" name="Password" id="Password" className='intext'/>
                 <br />
                 <input type="submit" value="Submit" />
+                <hr />
+                <div className="new">
+                <p>If New Member </p><a href="/">Click Here</a>
+                </div>
               </div>
             </form>
         </div>
